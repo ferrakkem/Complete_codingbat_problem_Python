1ffc35735a4dd7b7b9f29a476e6c9b41c15022ac
ferrakkem <ferrakkem@gmail.com>
2020-04-17 03:09:03 -0400

    commit of array_count9() problem solved

--- app.py
+++ app.py
@@ -285,6 +285,37 @@ def string_splosion(str):
         result = result + str[:i+1]
     return result
 
-print(string_splosion('ab'))
+#print(string_splosion('ab'))
+
+'''
+Given a string, return the count of the number of times that 
+a substring length 2 appears in the string and also as 
+the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2
+'''
+
+def last2(string):
+    pass
+
+
+'''
+Given an array of ints, return the number of 9's in the array.
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3
+'''
+
+def array_count9(number):
+    count = 0
+    for i in number:
+        if i == 9:
+            count = count + 1
+    return count
+
+print(array_count9([1, 9, 9, 3, 9,9]))
 
 
