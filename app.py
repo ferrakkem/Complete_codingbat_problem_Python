fdb6f2187da192e174eeb3402632ff045ac61c84
ferrakkem <ferrakkem@gmail.com>
2020-04-17 01:13:18 -0400

    commit of problem -- makes10()

--- app.py
+++ app.py
@@ -91,4 +91,21 @@ def parrot_trouble(hour, is_talking):
     else:
         return False
 
-print(parrot_trouble(True, 7))
+#print(parrot_trouble(True, 7))
+
+'''
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → True
+makes10(9, 9) → False
+makes10(1, 9) → True
+'''
+
+def makes10(a, b):
+
+    if (a == 10) or (b == 10) or (a+b == 10):
+        return True
+    else:
+        return False
+
+print(makes10(1, 9))
