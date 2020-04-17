d2444faf07e1a6549e468452d203529e9c3fea65
Md.Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2020-04-17 00:50:03 -0400

    monkey_trouble() problem done

--- app.py
+++ app.py
@@ -14,4 +14,25 @@ def sleep_in(weekday, vacation):
     else:
         False
 
-print(sleep_in(False, False))
+#print(sleep_in(False, False))
+
+'''
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
+We are in trouble if they are both smiling or if neither of them is smiling. 
+Return True if we are in trouble.
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+'''
+
+def monkey_trouble(a_smile, b_smile):
+
+    if a_smile and b_smile:
+        return True
+    elif not a_smile and not b_smile:
+        return True
+    else:
+        return True
+
+print(monkey_trouble(False, True))
