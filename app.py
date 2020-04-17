4f52c363e6968ed9de9fdfa419bb8017c008d79f
ferrakkem <ferrakkem@gmail.com>
2020-04-17 00:59:08 -0400

    return the absolute difference between n and 21

--- app.py
+++ app.py
@@ -54,5 +54,22 @@ def sum_double(num1, num2):
         sum = num1+num2
         return sum
 
-print(sum_double(2, 2))
+#print(sum_double(2, 2))
 
+'''
+Given an int n, return the absolute difference between n and 21, 
+except return double the absolute difference if n is over 21.
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+'''
+
+def diff21(n):
+
+    if n <= 21:
+        return 21 - n
+    else:
+        return (n - 21) * 2
+
+print(diff21(66))
