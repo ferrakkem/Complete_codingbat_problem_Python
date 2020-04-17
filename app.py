fe119d1e4631451692db443b3a26553af180fcd3
ferrakkem <ferrakkem@gmail.com>
2020-04-17 02:13:16 -0400

    commit of string_times() problem solved

--- app.py
+++ app.py
@@ -217,4 +217,21 @@ def front3(string):
     return get_front_three*3
 
 
-print(front3('Chocolate'))
+#print(front3('Chocolate'))
+
+'''
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi
+'''
+
+def string_times(string, number):
+
+    if (number>0):
+        return string*number
+    else:
+        return "You input negative number"
+
+print(string_times('Hi', -3))
