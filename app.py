e5d8014785d15aca2dc491679f63fa5b148ed4cf
ferrakkem <ferrakkem@gmail.com>
2020-04-17 01:52:55 -0400

    commit of problem -- missing_char()

--- app.py
+++ app.py
@@ -163,4 +163,22 @@ def not_string(sting):
     else:
         return sting
 
-print(not_string('Not bad') )
+#print(not_string('Not bad'))
+
+'''
+Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+The value of n will be a valid index of a char in the original string 
+(i.e. n will be in the range 0..len(str)-1 inclusive).
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+'''
+
+def missing_char(string, n):
+    front = string[:n]
+    back = string[n + 1:]
+    return front + back
+
+print(missing_char('kitten', 1))
+
