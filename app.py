4191025ed06e6de9760a954fef46c11dca90f61f
ferrakkem <ferrakkem@gmail.com>
2020-04-17 01:24:25 -0400

    commit of problem -- pos_neg()

--- app.py
+++ app.py
@@ -127,4 +127,21 @@ def near_hundred(n):
     else:
         return False
 
-print(near_hundred(90))
+#print(near_hundred(90))
+
+'''
+Given 2 int values, return True if one is negative and one is positive.
+Except if the parameter "negative" is True, then return True only if both are negative.
+
+pos_neg(1, -1, False) → True
+pos_neg(-1, 1, False) → True
+pos_neg(-4, -5, True) → True
+'''
+
+def pos_neg(a, b, negative):
+    if negative:
+        return (a < 0 and b < 0)
+    else:
+        return ((a < 0 and b > 0) or (a > 0 and b < 0))
+
+print(pos_neg(-1, -1, True))
