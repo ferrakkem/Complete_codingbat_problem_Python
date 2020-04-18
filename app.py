a2c13360f1886d82074889f6a621ce4e22650564
ferrakkem <ferrakkem@gmail.com>
2020-04-18 16:35:07 -0400

    commit of the same_first_last() problem solved

--- app.py
+++ app.py
@@ -495,5 +495,21 @@ def first_last6(number):
     else:
         return False
 
-print(first_last6([13, 6, 1, 2, 3]))
+#print(first_last6([13, 6, 1, 2, 3]))
 
+'''
+Given an array of ints, return True if the array is length 1 or more, 
+and the first element and the last element are equal.
+
+same_first_last([1, 2, 3]) → False
+same_first_last([1, 2, 3, 1]) → True
+same_first_last([1, 2, 1]) → True
+'''
+
+def same_first_last(num):
+    if num[0] == num[-1]:
+        return True
+    else:
+        return False
+
+print(same_first_last([1, 2, 1]))
