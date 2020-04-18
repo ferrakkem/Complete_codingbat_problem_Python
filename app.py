75b6030a9dc4b8780213e52fb3c77381fc6b3684
ferrakkem <ferrakkem@gmail.com>
2020-04-18 18:51:33 -0400

    commit of the cigar_party() problem solved

--- app.py
+++ app.py
@@ -541,4 +541,26 @@ reverse3([7, 0, 0]) → [0, 0, 7]
 def reverse3(number):
     return number[::-1]
 
-print(reverse3([1, 2, 3]))
+#print(reverse3([1, 2, 3]))
+####------------------------------------------------logic-1---------------------------------------------------------
+
+'''
+When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
+Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
+Return True if the party with the given values is successful, or False otherwise.
+
+cigar_party(30, False) → False
+cigar_party(50, False) → True
+cigar_party(70, True) → True
+
+'''
+
+def cigar_party(cigars,is_weekend):
+
+    if (cigars >= 40) and (cigars <=60) or(cigars >= 40) and is_weekend:
+        return True
+    else:
+        return False
+
+print(cigar_party(30,False))
