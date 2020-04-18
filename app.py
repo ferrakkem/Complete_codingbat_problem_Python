7ea5b12004522683d13f4beb7419459963d1836d
ferrakkem <ferrakkem@gmail.com>
2020-04-18 16:38:55 -0400

    commit of the common_end() problem solved

--- app.py
+++ app.py
@@ -512,4 +512,20 @@ def same_first_last(num):
     else:
         return False
 
-print(same_first_last([1, 2, 1]))
+#print(same_first_last([1, 2, 1]))
+
+'''
+Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. 
+Both arrays will be length 1 or more.
+
+common_end([1, 2, 3], [7, 3]) → True
+common_end([1, 2, 3], [7, 3, 2]) → False
+common_end([1, 2, 3], [1, 3]) → True
+'''
+def common_end(num1, num2):
+    if (num1[0]== num2[0]) or(num1[-1] == num2[-1]):
+        return True
+    else:
+        return False
+
+print(common_end([1, 2, 3], [1, 3]))
