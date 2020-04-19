1be8351f1963b9809775e6b4ef8b4261cd92d1ad
ferrakkem <ferrakkem@gmail.com>
2020-04-18 20:46:24 -0400

    commit of the squirrel_play() problem solved

--- app.py
+++ app.py
@@ -588,4 +588,29 @@ def date_fashion(you, my_date):
     else:
         return '1'
 
-print(date_fashion(5, 10))
+#print(date_fashion(5, 10))
+
+
+'''
+The squirrels in Palo Alto spend most of the day playing. 
+In particular, they play if the temperature is between 60 and 90 (inclusive). 
+Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, 
+return True if the squirrels play and False otherwise.
+
+squirrel_play(70, False) → True
+squirrel_play(95, False) → False
+squirrel_play(95, True) → True
+'''
+
+def squirrel_play(temperature, is_summer):
+
+    if (is_summer):
+        if (temperature >= 60) and (temperature <= 100):
+            return True
+    else:
+        if (temperature >= 60) and (temperature <= 90):
+            return True
+        else:
+            return False
+
+print(squirrel_play(95, True))
