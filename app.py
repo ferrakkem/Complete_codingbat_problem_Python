559efcefea52fc94294ca9d9c16f4f39a59b4c8d
ferrakkem <ferrakkem@gmail.com>
2020-04-18 21:04:16 -0400

    commit of the sorta_sum() problem solved

--- app.py
+++ app.py
@@ -645,5 +645,24 @@ def caught_speeding(speed, is_birthday):
         else:
             return 2
 
+#print(caught_speeding(60, False))
 
-print(caught_speeding(60, False))
+
+'''
+Given 2 ints, a and b, return their sum. 
+However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+'''
+
+def sorta_sum(a, b):
+    sum = a+b
+    if sum in range(10, 19):
+        return '20'
+    else:
+        return sum
+
+print(sorta_sum(19, 4))
+
