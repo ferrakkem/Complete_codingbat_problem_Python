e692cb19fcae8e69452d9e590ef0362b07a77849
ferrakkem <ferrakkem@gmail.com>
2020-04-20 21:08:06 -0400

    commit of the lone_sum() problem solved

--- app.py
+++ app.py
@@ -734,7 +734,50 @@ def near_ten(num):
     else:
         return False
 
-print(near_ten(17))
+#print(near_ten(17))
+
+##--------------------------------logic-2-----------------------------------##
+
+'''
+We want to make a row of bricks that is goal inches long. 
+We have a number of small bricks (1 inch each) and big bricks (5 inches each). 
+Return True if it is possible to make the goal by choosing from the given bricks. 
+This is a little harder than it looks and can be done without any loops.
+
+make_bricks(3, 1, 8) → True
+make_bricks(3, 1, 9) → False
+make_bricks(3, 2, 10) → True
+'''
+def make_bricks():
+    pass
+
+
+'''
+Given 3 int values, a b c, return their sum. 
+However, if one of the values is the same as another of the values, 
+it does not count towards the sum.
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+'''
+
+def lone_sum(a, b, c):
+
+    if a == b and b == c:
+        return 0
+    elif a == b:
+        return c
+    elif a == c:
+        return b
+    elif b == c:
+        return a
+    else:
+        return a+b+c
+
+print(lone_sum(1, 2, 3))
+
+
 
 
 
