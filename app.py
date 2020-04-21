1c92904aa18fc2c835fa4dfb06a42c26d5302f2e
ferrakkem <ferrakkem@gmail.com>
2020-04-21 19:57:17 -0400

    commit of the no_teen_sum() problem solved

--- app.py
+++ app.py
@@ -797,9 +797,47 @@ def lucky_sum(a, b, c):
     else:
         return a+b+c
 
-print(lucky_sum(1, 13, 3))
+#print(lucky_sum(1, 13, 3))
+
+'''
+Given 3 int values, a b c, return their sum. 
+However, if any of the values is a teen -- in the range 13..19 inclusive 
+-- then that value counts as 0, except 15 and 16 do not count as a teens. 
+Write a separate helper "def fix_teen(n):"that takes in an int value and returns that value fixed for the teen rule. 
+In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). 
+Define the helper below and at the same indent level as the main no_teen_sum().
+
+no_teen_sum(1, 2, 3) → 6
+no_teen_sum(2, 13, 1) → 3
+no_teen_sum(2, 1, 14) → 3
+'''
+
+def no_teen_sum(a, b, c):
+    return fix_teen(a) + fix_teen(b) +fix_teen(c)
 
 
+def fix_teen(n):
+    if  n >=13 and n <= 19 and n !=15 and n!= 16:
+        return 0
+    return n
+
+print(no_teen_sum(2, 1, 14))
+
+'''
+For this problem, we'll round an int value up to the next multiple of 10 
+if its rightmost digit is 5 or more, so 15 rounds up to 20. 
+Alternately, round down to the previous multiple of 10 
+if its rightmost digit is less than 5, so 12 rounds down to 10. 
+Given 3 ints, a b c, return the sum of their rounded values. 
+To avoid code repetition, write a separate helper "def round10(num):" 
+and call it 3 times. Write the helper entirely below and at the same indent level as round_sum().
+
+round_sum(16, 17, 18) → 60
+round_sum(12, 13, 14) → 30
+round_sum(6, 4, 4) → 10
+'''
+def round_sum():
+    pass
 
 
 
