5cd6ae4415e1f0db217528435f35ddd5dd2d298b
ferrakkem <ferrakkem@gmail.com>
2020-04-25 17:20:29 -0400

    commit of the cat_dog()  count problem solved

--- app.py
+++ app.py
@@ -905,4 +905,26 @@ def count_hi(string):
         if string[i] =='h' and string[i+1] == 'i':
             cout = cout+1
     return cout
-print(count_hi('hihi'))
+#print(count_hi('hihi'))
+
+'''
+Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True
+'''
+
+def cat_dog(string):
+    cat_count = 0
+    dog_count = 0
+
+    cat_count = string.count('cat')
+    dog_count = string.count('dog')
+
+    if cat_count == dog_count:
+        return True
+    else:
+        return False
+
+print(cat_dog('catcat'))
