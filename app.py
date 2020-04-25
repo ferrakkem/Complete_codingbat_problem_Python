09d2887f1f02e5cba19ec102bf3f8bd29db91398
ferrakkem <ferrakkem@gmail.com>
2020-04-25 17:28:44 -0400

    commit of the count_code()  count problem solved

--- app.py
+++ app.py
@@ -927,4 +927,23 @@ def cat_dog(string):
     else:
         return False
 
-print(cat_dog('catcat'))
+#print(cat_dog('catcat'))
+
+'''
+Return the number of times that the string "code" appears anywhere in the given string, 
+except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+count_code('aaacodebbb') → 1
+count_code('codexxcode') → 2
+count_code('cozexxcope') → 2
+'''
+
+def count_code(string):
+    count = 0
+
+    for i in range(len(string)-1):
+        if string[i] == 'c' and string[i+1] =='o' and string[i+3] == 'e':
+            count = count+1
+    return count
+
+print(count_code('cozexxcope'))
