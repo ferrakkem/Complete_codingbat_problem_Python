a48a3fb02df1167ecdde5ab1ca6faf93f5ece30c
ferrakkem <ferrakkem@gmail.com>
2020-04-26 17:49:56 -0400

    commit of the sum14() problem solved

--- app.py
+++ app.py
@@ -988,7 +988,7 @@ def xyz_there(string):
     else:
         return True
 
-print(xyz_there('abcxyz'))
+#print(xyz_there('.xyzabc'))
 
 #--------------------------list-2---------------------------------------------------------
 '''
@@ -1006,4 +1006,63 @@ def count_evens(numbers):
             count = count +1
     return count
 
-print(count_evens([1, 3, 5]))
+#print(count_evens([1, 3, 5]))
+
+'''
+Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. 
+Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+
+big_diff([10, 3, 5, 6]) → 7
+big_diff([7, 2, 10, 9]) → 8
+big_diff([2, 10, 7, 2]) → 8
+'''
+
+def big_diff(numbers):
+    pass
+
+
+big_diff([10, 3, 5, 6])
+
+
+'''
+Return the "centered" average of an array of ints, which we'll say is the mean average of the values, 
+except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, 
+ignore just one copy, and likewise for the largest value. Use int division to produce the final average. 
+You may assume that the array is length 3 or more.
+
+centered_average([1, 2, 3, 4, 100]) → 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+centered_average([-10, -4, -2, -4, -2, 0]) → -3
+'''
+
+'''
+Return the sum of the numbers in the array, returning 0 for an empty array. 
+Except the number 14 is very unlucky, so it does not count and numbers that come immediately after a 14 also do not count.
+
+sum14([1, 2, 2, 1]) → 6
+sum14([1, 1]) → 2
+sum14([1, 2, 2, 1, 14]) → 6
+
+'''
+def sum14(numbers):
+    sum = 0
+    #i = 0
+
+    for i in range(len(numbers)-1):
+        if numbers[i] == 14:
+            numbers.pop(i)
+        sum = sum + numbers[i]
+
+    return sum
+
+print(sum14([1, 2,1,1,2,14,1,1]))
+
+'''
+Return the sum of the numbers in the array, 
+except ignore sections of numbers starting with a 6 and extending to the next 7 
+(every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+'''
